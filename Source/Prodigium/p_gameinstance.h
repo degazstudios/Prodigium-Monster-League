221ece5c0524e0e37dc9be235ca846455240acab
Josh Degazio <jdegazio@outlook.com>
2024-01-30 23:17:12 -0500

    Refix Github commits

---
+++ Source/Prodigium/p_gameinstance.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/GameInstance.h"
+#include "p_gameinstance.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PRODIGIUM_API Up_gameinstance : public UGameInstance
+{
+	GENERATED_BODY()
+	
+};
---
+++ Source/Prodigium/testClass.cpp
@@ -0,0 +1,12 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "testClass.h"
+
+testClass::testClass()
+{
+}
+
+testClass::~testClass()
+{
+}
